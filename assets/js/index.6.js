a61a04dd2d183e5dc36ef37b145eacbb0242eb97
Gaurav Gahukar <gauravgahukar09@gmail.com>
2017-11-30 05:30:50 -0700

    sixth block

--- assets/js/index.6.js
+++ assets/js/index.6.js
@@ -1,24 +1,124 @@
-function calculate_transition_curve() {
+function calculate_transition_summit_curve() {
     
-        v = document.getElementById("fifth_velocity_one").value;
-        r = document.getElementById("fifth_radius_one").value;
-        w = document.getElementById("fifth_width_one").value;
-        e = document.getElementById("fifth_winding_one").value;
-        m = document.getElementById("fifth_elivation_one").value;
-    
-        v=parseFloat(v);
-        r=parseFloat(r);
-        w=parseFloat(w);
-        e=parseFloat(e);
-        m=parseFloat(m);
+        var ssd = document.getElementById("sixth_safe_sight_distance_one").value;
+        var n = document.getElementById("sixth_daviation_one").value;
+        var osd = document.getElementById("sixth_safe_osd").value;
+
+
+
+        ssd=parseFloat(ssd);
+        n=parseFloat(n);
+        osd=parseFloat(osd);
+
+
+
+
+        if(ssd>0) {
+
+        var l = ( n * ssd * ssd ) / 4.4 ;
         
-        z1 = (( w + r ) * ( v * m * e ) / 2);
-    
+               if ( l >= ssd) {
+                       var z1 = l ;
+               }
+               else if (l < ssd) {
+                   var z2 = 2*ssd - ( 4.4 / n ) ;
+       
+               }
+               else {
+                   var str="please enter the proper values : "
+               }
+           
+               var result = document.getElementById("result_block_sixth");
+               result.style.display = "block";
+           
+       
+               if ( l >= ssd) {
+                   document.getElementById("verticle_summit_curve").innerHTML = "Length of vertical curve ( Summit ) curve is : " +z1+" m.";
+                           }
+               else if (l < ssd) {
+                   document.getElementById("verticle_summit_curve").innerHTML = "Length of vertical curve ( Summit ) curve is : " +z2+" m.";
+               }
+               else {
+                   document.getElementById("verticle_summit_curve").innerHTML = " " + str;
+               }
+
+        }
+
+        else if(osd>0) {
+
+            
+            var l = ( n * osd * osd ) / 9.6 ;
         
-        
-        var result = document.getElementById("result_block_fifth");
-        result.style.display = "block";
-    
-        document.getElementById("result_infiltration").innerHTML = "Length of  transitional curve is : " +z1+" m.";
-    
+               if ( l >= osd) {
+                       var z1 = l ;
+               }
+               else if (l < osd) {
+                   var z2 = 2*osd - ( 9.6 / n ) ;
+       
+               }
+               else {
+                   var str="please enter the proper values : "
+               }
+           
+               var result = document.getElementById("result_block_sixth");
+               result.style.display = "block";
+           
+       
+               if ( l >= osd) {
+                   document.getElementById("verticle_summit_curve").innerHTML = "Length of vertical curve ( Summit ) curve is : " +z1+" m.";
+                           }
+               else if (l < osd) {
+                   document.getElementById("verticle_summit_curve").innerHTML = "Length of vertical curve ( Summit ) curve is : " +z2+" m.";
+               }
+               else {
+                   document.getElementById("verticle_summit_curve").innerHTML = " " + str;
+               }
+
+        }
+        else {
+            alert("Values are less than zero");
+        }
     }
+    //end fun
+
+
+    function calculate_transition_vally_curve() {
+        
+            var v = document.getElementById("sixth_velocity_two").value;
+            var r = document.getElementById("sixth_daviation_two").value;
+            var e = document.getElementById("sixth_safe_distance_two").value;
+        
+            v=parseFloat(v);
+            r=parseFloat(r);
+            e=parseFloat(e);
+            
+            
+
+            l = 0.38 * Math.sqrt(r*v*v*v);
+
+            var confart = l;
+
+            if(l>=e) {
+                l = (r*e*e)/(1.5+0.35*e);
+
+            }
+            else if(l<e) {
+                l=2*e-((1.5+0.035*e)/r);
+            }
+            else {
+                alert("not proper values");
+
+            }
+
+            var i =( 1.6 * e * v * v )/ l;
+                      
+            var result = document.getElementById("result_block_sixth");
+            result.style.display = "block";
+
+            document.getElementById("verticle_vally_curve").innerHTML = "Lenght of the valley curve ";
+
+            document.getElementById("verticle_vally_curve1").innerHTML = "According to confort condition : " +confart+" m.";
+            document.getElementById("verticle_vally_curve2").innerHTML = "According to headlight slight distance is : " +l+" m.";
+            document.getElementById("verticle_vally_curve3").innerHTML = "Impact factor is : " +l+" m.";
+        
+        }
